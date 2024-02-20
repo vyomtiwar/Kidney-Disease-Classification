cc772ee064bf49df2cde33437c17aa78e735ee65
Vyom <vyom.tiwari608@gmail.com>
2024-02-20 13:45:29 +0530

    prediction added

--- app.py
+++ app.py
@@ -1,8 +1,8 @@
 from flask import Flask, request, jsonify, render_template
 import os
 from flask_cors import CORS, cross_origin
-from cnnClassifier.utils.common import decodeImage
-from cnnClassifier.pipeline.prediction import PredictionPipeline
+from src.cnnClassifier.utils.common import decodeImage
+from src.cnnClassifier.pipeline.prediction import PredictionPipeline
 
 
 
